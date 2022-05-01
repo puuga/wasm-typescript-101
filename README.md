cd825223c7fe9a1f9d1534863b6c36f78a65addd
Siwawes Wongcharoen <siwaoh@gmail.com>
2022-05-01 21:20:49 +0700

    feature: wow

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # wasm-typescript-101
+
 TypeScript-like x WebAssembly
+
+## Setting up a new project
+
+<https://www.assemblyscript.org/getting-started.html#setting-up-a-new-project>
---
+++ assembly/index.ts
@@ -0,0 +1,9 @@
+// The entry file of your WebAssembly module.
+
+export function add(a: i32, b: i32): i32 {
+  return a + b;
+}
+
+export function fibonacci(a: i32): i32 {
+  return a < 2 ? a : fibonacci(a - 1) + fibonacci(a - 2);
+}
